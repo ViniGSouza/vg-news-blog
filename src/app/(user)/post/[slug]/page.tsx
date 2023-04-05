e06786b76a6262cd85f7acc75a91f73530285aa0
ViniGSouza <vinizintwitchtv@gmail.com>
2023-04-05 01:00:37 -0300

    sidebar component add

--- src/app/(user)/post/[slug]/page.tsx
+++ src/app/(user)/post/[slug]/page.tsx
@@ -7,6 +7,7 @@ import { RichTextComponents } from "@/components/RichTextComponents";
 import { BsArrowRightCircleFill } from "react-icons/bs";
 import Link from "next/link";
 import logo from "../../../../../public/logo.png";
+import SideBar from "@/components/SideBar";
 
 type Props = {
   params: {
@@ -62,76 +63,7 @@ async function Post({ params: { slug } }: Props) {
           </div>
           <PortableText value={post.body} components={RichTextComponents} />
         </div>
-        <div className="bg-[#0D0F12] mx-4 md:mx-0 md:ml-5 md:w-full md:max-w-[300px] max-h-[820px] shadow-md py-10 px-6">
-          <h3 className="text-3xl text-white">BUSCAR</h3>
-          <input
-            type="text"
-            placeholder="Digite aqui..."
-            className="bg-[#0d0f12] border-[#00bcff] border-[1px] rounded mt-3 w-full pl-2 py-1 text-white"
-          />
-          <h3 className="mt-10 mb-5 pb-3 text-3xl text-white border-b-[1px] border-[#00bcff]">DESTAQUES</h3>
-            <ul>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-            </ul>
-            <h3 className="mt-10 mb-5 pb-3 text-3xl text-white border-b-[1px] border-[#00bcff]">TUTORIAIS</h3>
-            <ul>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 1</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 2</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 3</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 4</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 5</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 6</p>
-              </Link>
-              </li>
-            </ul>
-        </div>
+        <SideBar />
       </div>
     </article>
   );
--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -4,8 +4,7 @@ import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 import ClientSideRoute from "./ClientSideRoute";
 import Banner from "./Banner/Banner";
 import Link from "next/link";
-import logo from "../../public/logo.png";
-import { BsArrowRightCircleFill } from "react-icons/bs";
+import SideBar from "./SideBar";
 
 type Props = {
   posts: Post[];
@@ -60,77 +59,8 @@ export default function BlogList({ posts }: Props) {
             <Link href="/" className="bg-[#00bcff] text-white px-6 py-3 rounded mt-5 hover:scale-105 transition-transform duration-200 ease-out">Ver mais notícias...</Link>
           </div>
         </div>
-
-        <div className="bg-[#0D0F12] mx-4 md:mx-0 md:ml-5 md:w-full md:max-w-[300px] max-h-[820px] shadow-md py-10 px-6">
-          <h3 className="text-3xl text-white">BUSCAR</h3>
-          <input
-            type="text"
-            placeholder="Digite aqui..."
-            className="bg-[#0d0f12] border-[#00bcff] border-[1px] rounded mt-3 w-full pl-2 py-1 text-white"
-          />
-          <h3 className="mt-10 mb-5 pb-3 text-3xl text-white border-b-[1px] border-[#00bcff]">DESTAQUES</h3>
-            <ul>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-              <li className="my-5">
-              <Link href="/">
-                <Image src={logo} alt="destaque"/>
-              </Link>
-              </li>
-            </ul>
-            <h3 className="mt-10 mb-5 pb-3 text-3xl text-white border-b-[1px] border-[#00bcff]">TUTORIAIS</h3>
-            <ul>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 1</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 2</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 3</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 4</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 5</p>
-              </Link>
-              </li>
-              <li className="my-2">
-              <Link href="/">
-                <p className="flex items-center text-lg text-white uppercase">
-                  <BsArrowRightCircleFill className="text-[#00bcff] mr-2" />Tutorial - 6</p>
-              </Link>
-              </li>
-            </ul>
-        </div>
+        
+        <SideBar />
       </div>
       
     </div>
